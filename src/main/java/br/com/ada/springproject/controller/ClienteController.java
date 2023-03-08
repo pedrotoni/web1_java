28efadf0133b9be501d825892820864b80e14f2b
Pedro de Toni <pedrovdetoni@gmail.com>
2023-03-08 11:17:22 -0300

    verificacao de caracteres do cpf e se cpf ja consta no database

--- src/main/java/br/com/ada/springproject/controller/ClienteController.java
+++ src/main/java/br/com/ada/springproject/controller/ClienteController.java
@@ -2,7 +2,9 @@ package br.com.ada.springproject.controller;
 
 import br.com.ada.springproject.model.Cliente;
 import br.com.ada.springproject.service.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -30,9 +32,39 @@ public class ClienteController {
     }
 
     @PostMapping("/add")
-    public String criarCliente(@ModelAttribute("cliente") Cliente cliente) {
-        this.clienteService.criaCliente(cliente);
+    public String criarCliente(@Valid @ModelAttribute("cliente")
+                                   Cliente cliente, Model model) {
+        if (!validaCPF(cliente.getCpf())) {
+            model.addAttribute("erro",
+                    "CPF deve conter 11 caracteres numéricos");
+            return "cliente-add";
+        }
+
+        try {
+            this.clienteService.criaCliente(cliente);
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("erro",
+                    "Este CPF já consta em nossa base de dados.");
+            return "cliente-add";
+        }
         return "redirect:/clientes";
+    }
+
+    public boolean validaCPF(String cpf) {
+        if (cpf == null) {
+            return false;
+        }
+
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        for (int digito = 0; digito < cpf.length(); digito++) {
+            if (!Character.isDigit(cpf.charAt(digito))) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }

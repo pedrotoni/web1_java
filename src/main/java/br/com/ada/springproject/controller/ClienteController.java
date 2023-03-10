d58afb67fd9ddd9c0ea7bacd1bbbb06f61fe0dd7
Pedro de Toni <pedrovdetoni@gmail.com>
2023-03-09 23:08:40 -0300

    Melhorias nas verificacoes do metodo POST, aplicacao das mesmas verificacoes no metodo PUT, adicao de botao para voltar a home page.

--- src/main/java/br/com/ada/springproject/controller/ClienteController.java
+++ src/main/java/br/com/ada/springproject/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,10 +36,18 @@ public class ClienteController {
 
     @PostMapping("/add")
     public String criarCliente(@Valid @ModelAttribute("cliente")
-                                   Cliente cliente, Model model) {
+                               Cliente cliente, BindingResult result, Model model) {
         if (!validaCPF(cliente.getCpf())) {
             model.addAttribute("erro",
                     "CPF deve conter 11 caracteres numéricos");
+            model.addAttribute("add", Boolean.TRUE);
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("formAction", "/clientes/add");
+            return "cliente-add";
+        }
+        if (result.hasErrors()) {
+            model.addAttribute("add", Boolean.TRUE);
+            model.addAttribute("formAction", "/clientes/add");
             return "cliente-add";
         }
 
@@ -47,20 +56,22 @@ public class ClienteController {
         } catch (DataIntegrityViolationException e) {
             model.addAttribute("erro",
                     "Este CPF já consta em nossa base de dados.");
+            model.addAttribute("add", Boolean.TRUE);
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("formAction", "/clientes/add");
             return "cliente-add";
         }
         return "redirect:/clientes";
     }
 
+
     public boolean validaCPF(String cpf) {
         if (cpf == null) {
             return false;
         }
-
         if (cpf.length() != 11) {
             return false;
         }
-
         for (int digito = 0; digito < cpf.length(); digito++) {
             if (!Character.isDigit(cpf.charAt(digito))) {
                 return false;
@@ -77,23 +88,49 @@ public class ClienteController {
 
     @GetMapping("/{clienteId}/edit")
     public String mostraEdicaoCliente(Model model,
-                                @PathVariable("clienteId") Long clienteId) {
+                                      @PathVariable("clienteId") Long clienteId) {
         Optional<Cliente> optionalCliente =
                 this.clienteService.buscaClientePorId(clienteId);
         optionalCliente.ifPresent(cliente ->
-                model.addAttribute("cliente",cliente));
+                model.addAttribute("cliente", cliente));
         model.addAttribute("add", Boolean.FALSE);
+        model.addAttribute("formAction", "/clientes/" + clienteId + "/edit");
         return "cliente-add";
     }
 
     @PutMapping("/{clienteId}/edit")
-    public String editarCliente(@ModelAttribute Cliente cliente,
-                                @PathVariable("clienteId") Long clienteId) {
-        cliente.setId(clienteId);
-        this.clienteService.criaCliente(cliente);
+    public String editarCliente(@Valid @ModelAttribute Cliente cliente,
+                                BindingResult result,
+                                @PathVariable("clienteId") Long clienteId,
+                                Model model) {
+
+        if (!validaCPF(cliente.getCpf())) {
+            model.addAttribute("erro",
+                    "CPF deve conter 11 caracteres numéricos");
+            model.addAttribute("add", Boolean.FALSE);
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("formAction", "/clientes/" + clienteId + "/edit");
+            return "cliente-add";
+        }
+
+        if (result.hasErrors()) {
+            model.addAttribute("add", Boolean.FALSE);
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("formAction", "/clientes/" + clienteId + "/edit");
+            return "cliente-add";
+        }
+
+        try {
+            cliente.setId(clienteId);
+            this.clienteService.criaCliente(cliente);
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("erro",
+                    "Este CPF já consta em nossa base de dados.");
+            model.addAttribute("add", Boolean.FALSE);
+            model.addAttribute("cliente", cliente);
+            model.addAttribute("formAction", "/clientes/" + clienteId + "/edit");
+            return "cliente-add";
+        }
         return "redirect:/clientes";
     }
-
-
-
 }
--- src/main/java/br/com/ada/springproject/model/Cliente.java
+++ src/main/java/br/com/ada/springproject/model/Cliente.java
@@ -1,6 +1,8 @@
 package br.com.ada.springproject.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,8 +18,11 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 3, message = "Nome deve ter ao menos 3 caracteres.")
+    @NotEmpty(message = "Nome não pode estar vazio.")
     private String nome;
 
+    @NotEmpty(message = "CPF não pode estar vazio.")
     @Column(unique = true)
     private String cpf;
 
